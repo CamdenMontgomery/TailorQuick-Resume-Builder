f7ae3ac25d29c2861a7f517de552158fa9b68d3f
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-10 12:43:43 -0400

    Styled project subsection. Fixed bug with bulleted lists not storing properly

--- src/components/SubsectionItems/ProjectsSubsection.tsx
+++ src/components/SubsectionItems/ProjectsSubsection.tsx
@@ -1,10 +1,11 @@
-import { Stack, Button, Heading, Grid, GridItem, Input } from "@chakra-ui/react"
+import { Stack, Heading, Grid, GridItem, Input } from "@chakra-ui/react"
 import { Field } from "../ui/field"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
 import { useDispatch } from "react-redux";
 import EditableBulletedList from "../ui/EditableBullets";
 import type Project from "../../interfaces/Project";
+import { CgCalendarToday } from "react-icons/cg";
 
 
 export default function ProjectsSubsection({data, index} : {data : Project,index : number}) {
@@ -15,9 +16,9 @@ export default function ProjectsSubsection({data, index} : {data : Project,index
     return (
         <Stack className="subsectionitem" >
 
-            <Heading fontFamily='WorkSans' textAlign='justify' color='black'>Project {index}</Heading>
+            <Heading textAlign='justify' color='black'>Project {index}</Heading>
 
-            <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(3, 1fr)" gap="1rem" alignItems="end">
+            <Grid templateRows="repeat(2, 1fr)" templateColumns="repeat(2, 1fr)" gap="1rem" alignItems="end">
 
                 <GridItem colSpan={2}>
                     <Field label="Title" background="white" color="gray" required>
@@ -26,17 +27,21 @@ export default function ProjectsSubsection({data, index} : {data : Project,index
                 </GridItem>
 
 
-                <GridItem>
-                    <DatePicker showIcon dateFormat="MMM yyyy" showMonthYearPicker placeholderText="e.g Jan. 1997" />
+              <GridItem>
+                    <Field label="Start Date" background="white" color="gray">
+                        <DatePicker value={new Date(data.startDate ?? 0).toLocaleDateString("en-US", { month: 'short', year: 'numeric' })} wrapperClassName="subsection-datepicker-wrapper" className="subsection-datepicker" showMonthYearPicker showIcon icon={<CgCalendarToday className="subsection-datepicker-icon" />} onChange={(date) => editField("EDIT_PROJECT_START_DATE", date?.valueOf())} dateFormat="MMM yyyy" placeholderText="e.g Jan 1997" />
+                    </Field>
                 </GridItem>
 
                 <GridItem>
-                    <DatePicker showIcon dateFormat="MMM yyyy" showMonthYearPicker placeholderText="e.g Jan. 1997" />
+                    <Field label="End Date" background="white" color="gray">
+                        <DatePicker value={new Date(data.endDate ?? 0).toLocaleDateString("en-US", { month: 'short', year: 'numeric' })} wrapperClassName="subsection-datepicker-wrapper" className="subsection-datepicker" showMonthYearPicker showIcon icon={<CgCalendarToday className="subsection-datepicker-icon" />} onChange={(date) => editField("EDIT_PROJECT_END_DATE", date?.valueOf())} dateFormat="MMM yyyy" placeholderText="e.g Jan 1997" />
+                    </Field>
                 </GridItem>
 
             </Grid>
-            <EditableBulletedList bullets={data.bullets ?? []} callback={(bullets : string[]) => { editField("EDIT_PROJECTS_BULLETS", bullets) }}></EditableBulletedList>
-            <Button></Button>
+            <EditableBulletedList bullets={data.bullets ?? []} callback={(bullets : string[]) => { editField("EDIT_PROJECT_BULLETS", bullets) }}></EditableBulletedList>
+
         </Stack>
     )
 }
