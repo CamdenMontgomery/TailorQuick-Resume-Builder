c64aaedf145ac532ebd23e5a386ab21faa04ad1e
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-09 21:47:34 -0400

    Styled resume preview modal

--- src/components/cover/Cover.tsx
+++ src/components/cover/Cover.tsx
@@ -1,11 +1,11 @@
-import { AbsoluteCenter, Center, Heading, HStack, ProgressCircle, VStack, Text, Flex, Tag, Button, Spinner } from "@chakra-ui/react";
+import { Center, Heading, HStack, VStack, Text, Flex, Tag, Button, Spinner, FormatNumber, Progress, Stat, Badge } from "@chakra-ui/react";
 import "./Cover.css"
 import ResumePreview from "./ResumePreview";
 import { useEffect, useState } from "react";
 import type { IResume } from "../../interfaces/IResume";
 //import type { ResumePreviewMessageTypes } from "../../types/ResumePreviewMessageTypes";
 import type { ResumePreviewModes } from "../../types/ResumePreviewModes";
-
+import { RxCross2 } from "react-icons/rx";
 
 export default function ResumePreviewCover() {
     const [resume, setResume] = useState<IResume | undefined>(undefined)
@@ -30,7 +30,7 @@ export default function ResumePreviewCover() {
                     break;
                 }
                 case 'ERROR': { break; }
-                case 'PASS_RESUME': { 
+                case 'PASS_RESUME': {
                     setMode('PREVIEW')
                     setResume(message.data.payload);
                     break
@@ -63,23 +63,30 @@ export default function ResumePreviewCover() {
                         {resume && <ResumePreview resume={resume}></ResumePreview>}
                     </Center>
                     <VStack className="resume-preview-modal-sidepanel">
-
+                        <Flex width="100%" justifyContent="end"><Button variant="ghost"><RxCross2 /></Button></Flex>
+                        
+                        
                         <Heading className="resume-preview-modal-title">Resume Generation Successful</Heading>
+                        <Text className="resume-preview-modal-subtext">We've generated a resume fitting the job description based on your personalized information. Here's the results!</Text>
+                        
                         <HStack className="resume-preview-modal-stats">
-                            <ProgressCircle.Root size='lg' value={5}>
-                                <ProgressCircle.Circle>
-                                    <ProgressCircle.Track />
-                                    <ProgressCircle.Range />
-                                </ProgressCircle.Circle>
-                                <AbsoluteCenter>
-                                    <ProgressCircle.ValueText />
-                                </AbsoluteCenter>
-                            </ProgressCircle.Root>
-
-                            <VStack>
-                                <Heading>Job Relevance</Heading>
-                                <Text>Score Passable</Text>
-                            </VStack>
+                            <Stat.Root className="resume-preview-stat-card" gap="1rem">
+                                <Stat.Label>Relevance Score</Stat.Label>
+                                <HStack gap="1rem">
+                                    <Stat.ValueText fontSize="xxx-large">
+                                        <FormatNumber value={0.7} style="percent" />
+                                    </Stat.ValueText>
+                                    <Badge colorPalette="green" gap="0">
+                                        Good Candidate
+                                    </Badge>
+                                </HStack>
+                                <Stat.HelpText color="gray" mb="2">You have an above average change of hiring</Stat.HelpText>
+                                <Progress.Root value={70} striped animated>
+                                    <Progress.Track background="#f9f9f9">
+                                        <Progress.Range background="#a9e7c2"/>
+                                    </Progress.Track>
+                                </Progress.Root>
+                            </Stat.Root>
                         </HStack>
 
                         <VStack className="resume-preview-modal-improvements">
@@ -91,7 +98,7 @@ export default function ResumePreviewCover() {
                             </Flex>
                         </VStack>
 
-                        <Button className="resume-preview-modal-update-button" variant='outline'>Update Transcript</Button>
+                        <Button className="resume-preview-modal-button" variant='outline'>Update Transcript</Button>
 
                     </VStack>
                 </HStack>
