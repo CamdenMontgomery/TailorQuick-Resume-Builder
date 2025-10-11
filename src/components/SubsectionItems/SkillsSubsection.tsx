c7beaec1eaa26246d63c436f0c3c9cf44b980548
Camden Montgomery <camden.montgomery@sargas.ai>
2025-10-11 13:19:30 -0400

    Add support for 'Enter' key used to submit new skill to list

--- src/components/SubsectionItems/SkillsSubsection.tsx
+++ src/components/SubsectionItems/SkillsSubsection.tsx
@@ -1,5 +1,5 @@
 import { Button, Flex, Heading, HStack, Input, Stack, Tag } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, type KeyboardEvent } from "react";
 import { useDispatch } from "react-redux";
 
 export default function SkillsSubsection({ data }: { data: string[] }) {
@@ -7,19 +7,28 @@ export default function SkillsSubsection({ data }: { data: string[] }) {
     const dispatch = useDispatch()
     const inputRef = useRef<HTMLInputElement>(null)
 
+    const addSkillFromInput = () => {
+        
+       if (inputRef.current && inputRef.current.value != "") dispatch({ type: "ADD_SKILL", payload: { value: inputRef.current.value } })
+    }
+
+    const removeSkill = (skill : string) => {
+        dispatch({ type: "REMOVE_SKILL", payload: { value: skill } })
+    }
+
     return (
         <Stack className="subsectionitem" margin="3rem">
             <Heading textAlign='justify' color='black'>Skills</Heading>
             <HStack>
-                <Input placeholder="Write down a skill you have..." ref={inputRef}></Input>
-                <Button background={"black"} color={"white"} onClick={() => { if (inputRef.current && inputRef.current.value != "") dispatch({ type: "ADD_SKILL", payload: { value: inputRef.current.value } }) }}>Add</Button>
+                <Input placeholder="Write down a skill you have..." ref={inputRef} onKeyDown={(event: KeyboardEvent) => { if ( event.key == 'Enter' ) addSkillFromInput() }}></Input>
+                <Button background={"black"} color={"white"} onClick={ addSkillFromInput }>Add</Button>
             </HStack>
             <Flex gap={0.5} className="skill-subsection-skill-container">
                 {(data.sort()).map((skill: string) =>
                     <Tag.Root size="lg" colorPalette="orange" height={"fit-content"}>
                         <Tag.Label textTransform="capitalize">{skill}</Tag.Label>
                         <Tag.EndElement>
-                            <Tag.CloseTrigger onClick={() => dispatch({ type: "REMOVE_SKILL", payload: { value: skill } })} />
+                            <Tag.CloseTrigger onClick={ () => removeSkill(skill) } />
                         </Tag.EndElement>
                     </Tag.Root>
                 )}
